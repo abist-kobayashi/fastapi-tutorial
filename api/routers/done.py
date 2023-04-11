a882bd741180569ec0933b1ce5ea40b0d3cd072b
kobayashi/personal <ikuyorih.kobayashi@gmail.com>
2023-04-11 22:33:22 +0900

    CRUD taskリソースまで

--- api/routers/done.py
+++ api/routers/done.py
@@ -2,10 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("tasks/{task_id}/done")
+@router.get("tasks/{task_id}/done", response_model= None)
 async def mark_task_as_done(task_id: int):
-    pass
+    return
 
-@router.delete("tasks/{task_id}/done")
+@router.delete("tasks/{task_id}/done", response_model= None)
 async def unmark_task_as_done(task_id: int):
-    pass
+    return
